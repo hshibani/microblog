4765ad4ee9bbb11c57c4aa0a08a9b332e828e5c3
hshibani <mercury26th@gmail.com>
2018-07-24 12:28:16 -0700

    adding logging mechanism

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 
 
 # Creating an instance of the application, naming it app
@@ -20,6 +24,19 @@ login = LoginManager(app)
 # after attempting to access a protected page(one with login_required decorator set) without login
 login.login_view = 'login'
 
+# logging using Python's logging package
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
 # app here is the package name, routes is the class or a module, models is a module representing
 # the structure of the db
-from app import routes, models
+from app import routes, models, errors
